336b6cc60aba95307c966bbb5d2d456725977f79
wenqi0425 <xiaoc288@gmail.com>
2022-12-20 23:41:20 +0100

    services

---
+++ Services/EFServices/EFRecipeItemService.cs
@@ -0,0 +1,33 @@
+﻿using AddLineTest.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AddLineTest.Services.EFServices
+{
+    public class EFRecipeItemService
+    {
+        AppDbContext _context;
+        public EFRecipeItemService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddRecipeItem(RecipeItem recipeItem)
+        {
+            _context.RecipeItems.Add(recipeItem);
+            _context.SaveChanges();
+        }
+
+        public void DeleteRecipeItem(RecipeItem recipeItem)
+        {
+            _context.RecipeItems.Remove(recipeItem);
+            _context.SaveChanges();
+        }
+
+        public void EditRecipeItem(RecipeItem recipeItem)
+        {
+            _context.RecipeItems.Update(recipeItem);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Services/Interfaces/IRecipeItemService.cs
@@ -0,0 +1,13 @@
+﻿using AddLineTest.Model;
+using System.Collections.Generic;
+
+namespace AddLineTest.Services.Interfaces
+{
+    public interface IRecipeItemService
+    {
+        void AddRecipeItem(RecipeItem recipe);
+        void DeleteRecipeItem(RecipeItem recipe);
+        void EditRecipeItem(RecipeItem recipe);
+        IEnumerable<RecipeItem> GetRecipeItemsByRecipeId(int recipeId);
+    }
+}
