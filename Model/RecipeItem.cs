684268d908e9a291e634dc7f3634d6360e28d2b3
wenqi0425 <xiaoc288@gmail.com>
2022-12-20 23:20:01 +0100

    Add project files.

---
+++ Model/RecipeItem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AddLineTest.Model
+{
+    public class RecipeItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Ingredient Name:")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Amount:")]
+        public string Amount { get; set; }
+    }
+}
